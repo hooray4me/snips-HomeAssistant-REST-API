d7411ad60ddc09555c634721570012c779dc687e
Rob Hedrick <rob@hedrickholdings.com>
2019-06-18 14:58:52 -0500

    added conditional logic to check device state

--- action-HomeAssistant-REST-API.py
+++ action-HomeAssistant-REST-API.py
@@ -53,10 +53,15 @@ def action_wrapper(hermes, intentMessage, conf):
      if myDeviceId == "the tv" or myDeviceId == "the television":
        theDevice = "harmony.remote"
      if myState != "query":
-       payload = json.dumps({"entity_id": theDevice})
-       url = 'http://'+ myip.encode("utf-8") + ':' + myport.encode("utf-8") + '/api/services/homeassistant/turn_' + myState.encode("utf-8")
-       response = post(url, headers=header, data=payload)
-       hermes.publish_end_session(current_session_id, "Turning " + myState.encode("utf-8") + " " + myDeviceId.encode("utf-8"))
+       url = 'http://'+ myip.encode("utf-8") + ':' + myport.encode("utf-8") + '/api/states/' + theDevice
+       response = get(url, headers=header)
+       if response.json()['state'] != myState:
+         payload = json.dumps({"entity_id": theDevice})
+         url = 'http://'+ myip.encode("utf-8") + ':' + myport.encode("utf-8") + '/api/services/homeassistant/turn_' + myState.encode("utf-8")
+         response = post(url, headers=header, data=payload)
+         hermes.publish_end_session(current_session_id, "Turning " + myState.encode("utf-8") + " " + myDeviceId.encode("utf-8"))
+       else:
+         hermes.publish_end_session(current_session_id, "Be Boop Beep. Um, " = myDeviceId.encode("utf-8") = " is already turned " + myState.encode("utf-8"))
      else:
        url = 'http://'+ myip.encode("utf-8") + ':' + myport.encode("utf-8") + '/api/states/' + theDevice
        response = get(url, headers=header)
